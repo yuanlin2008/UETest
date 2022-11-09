a9a487ca4346b8e1ef5772aeba74ab9a10700a27
yuanlin2008 <yuanlin2008@hotmail.com>
2022-11-09 11:24:04 +0800

    Initial

---
+++ Source/UE5Test/UE5TestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5TEST_API AUE5TestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
